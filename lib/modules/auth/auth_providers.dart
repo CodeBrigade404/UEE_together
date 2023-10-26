84b92aca26e766da688f75d3b7c77dd2ebcc80b3
ManulaK <it21019968@my.sliit.lk>
2023-10-27 03:58:23 +0530

    uneasy

--- lib/modules/auth/auth_providers.dart
+++ lib/modules/auth/auth_providers.dart
@@ -1,6 +1,5 @@
 import 'package:event_booking_app/Firebase/firebase_authentication.dart';
 import 'package:event_booking_app/modules/user/user_ctrl.dart';
-import 'package:event_booking_app/modules/user/user_dashboard/screen/user_dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
