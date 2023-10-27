f3ce47f79bd50ca27b9e45f6b8a572be47a7311b
ManulaK <it21019968@my.sliit.lk>
2023-10-27 12:36:38 +0530

    artist list

--- lib/modules/user/user_flowing/artists_display_screen.dart
+++ lib/modules/user/user_flowing/artists_display_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unused_field
-
 import 'package:event_booking_app/data/following_artist_data.dart';
 import 'package:event_booking_app/models/following_model.dart';
 import 'package:event_booking_app/shared/appbars/default_appbar.dart';
@@ -14,7 +12,8 @@ class FollowingPage extends StatefulWidget {
 
 class _FollowingPageState extends State<FollowingPage>
     with TickerProviderStateMixin {
-  final List<FollowingArtist> _selectedUsers = [];
+  TextEditingController searchController = TextEditingController();
+  List<FollowingArtist> filteredArtist = List.from(artist);
 
   @override
   Widget build(BuildContext context) {
@@ -22,68 +21,115 @@ class _FollowingPageState extends State<FollowingPage>
       appBar: const CustomAppBar(
         title: 'My Followings',
       ),
-      body: Container(
-          padding: const EdgeInsets.only(right: 20, left: 20),
-          color: Colors.white,
-          height: double.infinity,
-          width: double.infinity,
-          child: ListView.builder(
-            itemCount: artist.length,
-            itemBuilder: (context, index) {
-              return userComponent(user: artist[index]);
-            },
-          )),
+      body: Column(
+        children: [
+          SearchBar(searchController: searchController, onSearch: onSearch),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredArtist.length,
+              itemBuilder: (context, index) {
+                return userComponent(user: filteredArtist[index]);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   userComponent({required FollowingArtist user}) {
     return Container(
-      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      padding: const EdgeInsets.only(top: 10, bottom: 10, left: 15, right: 15),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(children: [
-            SizedBox(
+          Row(
+            children: [
+              SizedBox(
                 width: 60,
                 height: 60,
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(50),
                   child: Image.network(user.image),
-                )),
-            const SizedBox(width: 10),
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text(user.name,
-                  style: const TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w500)),
-              const SizedBox(
-                height: 5,
+                ),
               ),
-              Text(user.username, style: TextStyle(color: Colors.grey[600])),
-            ])
-          ]),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    user.name,
+                    style: const TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    user.username,
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+            ],
+          ),
           Container(
-              height: 40,
-              decoration: BoxDecoration(
-                border: Border.all(color: const Color(0xffeeeeee)),
+            height: 40,
+            decoration: BoxDecoration(
+              border: Border.all(color: const Color(0xffeeeeee)),
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: MaterialButton(
+              elevation: 0,
+              color: user.isFollowedByMe
+                  ? const Color(0xffeeeeee)
+                  : const Color(0x00ffffff),
+              onPressed: () {
+                setState(() {
+                  user.isFollowedByMe = !user.isFollowedByMe;
+                });
+              },
+              shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(50),
               ),
-              child: MaterialButton(
-                elevation: 0,
-                color: user.isFollowedByMe
-                    ? const Color(0xffeeeeee)
-                    : const Color(0x00ffffff),
-                onPressed: () {
-                  setState(() {
-                    user.isFollowedByMe = !user.isFollowedByMe;
-                  });
-                },
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: Text(user.isFollowedByMe ? 'Unfollow' : 'Follow',
-                    style: const TextStyle(color: Colors.black)),
-              ))
+              child: Text(user.isFollowedByMe ? 'Unfollow' : 'Follow',
+                  style: const TextStyle(color: Colors.black)),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  void onSearch(String searchText) {
+    setState(() {
+      filteredArtist = artist
+          .where((artist) =>
+              artist.name.toLowerCase().contains(searchText.toLowerCase()) ||
+              artist.username.toLowerCase().contains(searchText.toLowerCase()))
+          .toList();
+    });
+  }
+}
+
+class SearchBar extends StatelessWidget {
+  final TextEditingController searchController;
+  final Function(String) onSearch;
+
+  const SearchBar(
+      {super.key, required this.searchController, required this.onSearch});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: searchController,
+        decoration: const InputDecoration(
+          hintText: 'Search for artists...',
+          prefixIcon: Icon(Icons.search),
+        ),
+        onChanged: (value) {
+          onSearch(value);
+        },
       ),
     );
   }
